70dded91a6db17bf271f8f14e0d9b0482b93d5be
wasm-forge <122647775+wasm-forge@users.noreply.github.com>
2025-08-05 07:01:40 +0200

    update build script

--- build.rs
+++ build.rs
@@ -7,12 +7,10 @@ fn main() {
     let sdk_path = if let Ok(sdk_path) = env::var("WASI_SDK") {
         sdk_path
     } else {
-        match std::env::consts::OS {
-            "linux" => "/opt/wasi-sdk".to_string(),
-            "macos" => "/opt/wasi-sdk".to_string(),
-            other => panic!("Unsupported OS: {other}"),
-        }
+        "/opt/wasi-sdk".to_string()
     };
+
+    println!("cargo:rustc-link-arg=--sysroot={sdk_path}/share/wasi-sysroot");
 
     let pattern = format!("{sdk_path}/lib/clang/*/lib/*wasip1");
 
@@ -25,12 +23,7 @@ fn main() {
         // use the latest version we have found
 
         println!("cargo:rustc-link-search={}", path.display());
-
-        match std::env::consts::OS {
-            "linux" => println!("cargo:rustc-link-arg=-lclang_rt.builtins"),
-            "macos" => println!("cargo:rustc-link-arg=-lclang_rt.builtins"),
-            other => panic!("Unsupported OS: {other}"),
-        }
+        println!("cargo:rustc-link-arg=-lclang_rt.builtins");
     } else {
         panic!("Could not find clang wasm32 builtins under '{pattern}'");
     }
