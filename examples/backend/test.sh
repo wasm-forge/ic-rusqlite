636d24095fd7b3a0da66a2acea27cd35fa07ab2d
wasm-forge <122647775+wasm-forge@users.noreply.github.com>
2026-04-12 15:15:48 +0200

    test build

--- examples/backend/test.sh
+++ examples/backend/test.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 set -e
 
-dfx canister call backend create
+dfx canister call backend query '("select RANDOM() as RANDOM")'
+dfx canister call backend query '("select RANDOM() as RANDOM")'
 
-dfx canister call backend insert '(record {name = "person1"; age = 23})'
+# dfx canister call backend create
 
-dfx canister call backend insert '(record {name = "person2"; age = 24})'
+# dfx canister call backend insert '(record {name = "person1"; age = 23})'
 
-dfx canister call backend insert '(record {name = "person3"; age = 25})'
+# dfx canister call backend insert '(record {name = "person2"; age = 24})'
 
-dfx canister call backend insert '(record {name = "person4"; age = 26})'
+# dfx canister call backend insert '(record {name = "person3"; age = 25})'
 
-dfx canister call backend insert '(record {name = "person5"; age = 27})'
+# dfx canister call backend insert '(record {name = "person4"; age = 26})'
 
-dfx canister call backend insert '(record {name = "person6"; age = 28})'
+# dfx canister call backend insert '(record {name = "person5"; age = 27})'
 
-dfx canister call backend query '("select * from person")'
+# dfx canister call backend insert '(record {name = "person6"; age = 28})'
+
+# dfx canister call backend query '("select * from person")'
 
 
